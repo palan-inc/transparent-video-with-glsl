2356a1b96668ef6ed1f810517a1197ee14e9bbc3
sasanda-panda <eishis-sasai@eishis-sasai-mac.local>
2021-12-02 04:13:15 +0900

    add: comment in modules

--- src/ts/modules/UIController.ts
+++ src/ts/modules/UIController.ts
@@ -1,20 +1,16 @@
 export default class UIController {
-  classNameLoad: string;
   classNameActive: string;
 
   constructor() {
-    this.classNameLoad = "is-load";
     this.classNameActive = "is-active";
   }
 
-  loadDOM(dom: HTMLElement) {
-    dom.classList.add(this.classNameLoad);
-  }
-
+  // MEMO: DOMを表示する関数
   activateDOM(dom: HTMLElement) {
     dom.classList.add(this.classNameActive);
   }
 
+  // MEMO: DOMを非表示する関数
   deactivateDOM(dom: HTMLElement) {
     dom.classList.remove(this.classNameActive);
   }
--- src/ts/modules/WebGLController.ts
+++ src/ts/modules/WebGLController.ts
@@ -5,14 +5,17 @@ export default class WebGLController {
     this.gl = null;
   }
 
+  // MEMO: WebGLコンテキストを初期化する関数
   initWebGLContext(canvas: HTMLCanvasElement) {
     this.gl = canvas.getContext("webgl");
   }
 
+  // MEMO: WebGLコンテキストを返す関数
   getWebGLContext() {
     return this.gl;
   }
 
+  // MEMO: テクスチャを初期化する関数
   initTexture() {
     const texture = this.gl.createTexture();
     this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
@@ -32,6 +35,7 @@ export default class WebGLController {
     return texture;
   }
 
+  // MEMO: テクスチャを更新する関数
   updateTexture(texture: WebGLTexture, video: HTMLVideoElement) {
     const level = 0;
     const internalFormat = this.gl.RGBA;
@@ -41,6 +45,11 @@ export default class WebGLController {
     this.gl.texImage2D(this.gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, video);
   }
 
+  // -----
+  // シェーダとは...？
+  // 3Dオブジェクトをディスプレイに描画するためのプログラム
+  // -----
+  // MEMO: シェーダを生成する関数
   createShader(type: "vertex" | "fragment", source: string) {
     let shader;
 
@@ -67,6 +76,12 @@ export default class WebGLController {
     }
   }
 
+  // -----
+  // プログラムオブジェクトとは...？
+  // vertexシェーダとfragmentシェーダを紐付けたり
+  // JavaScriptからGLSLへ変数を渡す役割をもつオブジェクトのこと
+  // ------
+  // MEMO: プログラムオブジェクトを生成しシェーダをリンクする関数
   createProgram(vertexShader: string, fragmentShader: string) {
     const program = this.gl.createProgram();
     this.gl.attachShader(program, vertexShader);
@@ -80,6 +95,11 @@ export default class WebGLController {
     }
   }
 
+  // -----
+  // VBOとは...？
+  // 頂点データの格納オブジェクトのこと
+  // ------
+  // MEMO: VBOを生成する関数
   createVbo(vboArray: number[]) {
     const vbo = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ARRAY_BUFFER, vbo);
@@ -88,6 +108,12 @@ export default class WebGLController {
     return vbo;
   }
 
+  // -----
+  // IBOとは...？
+  // 頂点データの肥大化を抑えるため、
+  // 頂点データを使い回すインデックスデータの格納オブジェクトのこと
+  // ------
+  // MEMO: IBOを生成する関数
   createIbo(iboArray: number[]) {
     const ibo = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, ibo);
